e5c930f2395c506d43e749c5d4d2fcdd0c2a663a
Khanh Pham <khanh.pham@ais-tech.vn>
2024-06-08 06:13:29 +0000

    implement condition of CREATE_UPDATE_DELETE method for Matches

--- capfootballmatch/srv/fm_srv.js
+++ capfootballmatch/srv/fm_srv.js
@@ -2,7 +2,42 @@ const cds = require('@sap/cds');
 
 class FMService extends cds.ApplicationService {
   init() {
-    const { Bets } = this.entities
+    const { Bets, Matches } = this.entities
+
+    this.before(['CREATE'], Matches, async req => {
+      const now = new Date();
+      if (new Date(req.data.match_time) <= now) {
+        return req.error(400, 'Overdue to create');
+      }
+    })
+
+    this.before(['UPDATE'], Matches, async req => {
+    
+      const { match_id } = req.params[0];
+      const match = await cds.tx(req).run(SELECT.one.from('football.match.Matches').where({ match_id }));
+      if (!match) {
+        return req.error(400, 'Match does not exist');
+      }
+
+      const now = new Date();
+      if (new Date(match.match_time) <= now) {
+        return req.error(400, 'Overdue to update');
+      }
+    })
+
+    this.before(['DELETE'], Matches, async req => {
+      
+      const { match_id } = req.params[0];
+      const match = await cds.tx(req).run(SELECT.one.from('football.match.Matches').where({ match_id }));
+      if (!match) {
+        return req.error(400, 'Match does not exist');
+      }
+
+      const now = new Date();
+      if (new Date(match.match_time) <= now) {
+        return req.error(400, 'Overdue to delete');
+      }
+    })
 
     this.before(['CREATE'], Bets, async req => {
       // only allow to create when the match in this bet not yet take place
