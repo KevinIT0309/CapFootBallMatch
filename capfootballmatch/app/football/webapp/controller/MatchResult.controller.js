16de15b51c3fb7d7651e032e5c7e45d7b5f89070
Tri Minh Le <tri.leminh@ais-tech.vn>
2024-06-16 07:25:09 +0000

    fix ui

--- capfootballmatch/app/football/webapp/controller/MatchResult.controller.js
+++ capfootballmatch/app/football/webapp/controller/MatchResult.controller.js
@@ -3,9 +3,12 @@ sap.ui.define([
     "sap/m/MessageToast",
     "sap/m/MessageBox",
     "../model/formatter",
-    "sap/ui/model/json/JSONModel"
-], function (BaseController, MessageToast, MessageBox, formatter, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/library"
+], function (BaseController, MessageToast, MessageBox, formatter, JSONModel, coreLibrary) {
     "use strict";
+
+    let ValueState = coreLibrary.ValueState;
 
     return BaseController.extend("cap.euro.admin.football.controller.MatchResult", {
         onInit: function () {
@@ -15,7 +18,20 @@ sap.ui.define([
         onPatternMatched: async function (oEvent) {
             let oData = {
                 "enabledSaveBtn": true,
-                "predictOptions": []
+                "enabledOtherInputs": true,
+                "enabledPredictInput": true,
+                "enabledMatchStatus": true,
+                "predictOptions": [],
+                "matchStatusList": [{
+                    "matchStatus": 1,
+                    "matchStatusDesc": "Waiting"
+                }, {
+                    "matchStatus": 2,
+                    "matchStatusDesc": "Ongoing"
+                }, {
+                    "matchStatus": 3,
+                    "matchStatusDesc": "Done"
+                }]
             };
 
             // set explored app's demo model on this sample
@@ -61,17 +77,38 @@ sap.ui.define([
                 const instant = new Date();
                 const matchDate = new Date(matchDateTime.toDateString());
                 const today = new Date(instant.toDateString());
-                if (matchDate <= today) {
-                    oModel.setProperty("/enabledSaveBtn", false);
-                } else {
-                    this._validateEnabledSaveBtn();
-                }
+                // if (matchDate < today) {
+                //     oModel.setProperty("/enabledSaveBtn", false);
+                //     oModel.setProperty("/enabledPredictInput", false);
+                //     oModel.setProperty("/enabledMatchStatus", false);
+                // } else {
+                    this._validateEnabledProperty();
+                // }
+            }
+        },
+
+        handleTeamWinChange: function (oEvent) {
+            let oValidatedComboBox = oEvent.getSource(),
+                sSelectedKey = oValidatedComboBox.getSelectedKey(),
+                sValue = oValidatedComboBox.getValue();
+
+            if (!sSelectedKey && sValue) {
+                oValidatedComboBox.setValueState(ValueState.Error);
+                oValidatedComboBox.setValueStateText("Please enter a valid country!");
+            } else {
+                oValidatedComboBox.setValueState(ValueState.None);
             }
         },
 
         handleSave: function () {
+            const bindingPath = this.getView().getBindingContext("mainModel").getPath();
+            const object = this.getView().getBindingContext("mainModel").getObject();
+            if (object.status === "3" || object.status === 3 && !object.isOver) { // Done
+                this.getView().getBindingContext("mainModel").setProperty(`${bindingPath}/isOver`, true, "UpdateGroup");
+            }
             let fnSuccess = function () {
                 MessageToast.show("Match Saved Successfully");
+                this.getRouter().navTo("matchList");
             }.bind(this);
 
             let fnError = function (oError) {
@@ -79,7 +116,6 @@ sap.ui.define([
             }.bind(this);
 
             this.getModel("mainModel").submitBatch("UpdateGroup").then(fnSuccess, fnError);
-            this.getRouter().navTo("matchList");
         },
 
         handleClose: function (oEvent) {
@@ -88,24 +124,41 @@ sap.ui.define([
         },
 
         handleScoreChange: function (oEvent) {
-            this._validateEnabledSaveBtn();
+            this._validateEnabledProperty();
         },
 
         handleNumberofPredictsChange: function () {
-            this._validateEnabledSaveBtn();
+            this._validateEnabledProperty();
         },
 
-        _validateEnabledSaveBtn: function () {
+        handleMatchStatusChange: function () {
+            this._validateEnabledProperty();
+        },
+
+        _validateEnabledProperty: function () {
+            let viewModel = this.getModel("viewModel");
             const object = this.getView().getBindingContext("mainModel").getObject();
+            const status = object.status;
             const isOver = object.isOver;
             if (isOver) {
-                this.getModel("viewModel").setProperty("/enabledSaveBtn", false);
+                viewModel.setProperty("/enabledSaveBtn", false);
+                viewModel.setProperty("/enabledOtherInputs", false);
+                viewModel.setProperty("/enabledPredictInput", false);
+                viewModel.setProperty("/enabledMatchStatus", false);
             } else {
-                const team1_score = object.team1_score;
-                const team2_score = object.team2_score;
-                const predicts = object.predicts;
-                let enabledSaveBtn =  (!isNaN(parseInt(team1_score)) && team1_score >= 0) || (!isNaN(parseInt(team2_score)) && team2_score >= 0) || (!isNaN(parseInt(predicts)) && predicts >= 0) ? true : false;
-                this.getModel("viewModel").setProperty("/enabledSaveBtn", enabledSaveBtn);
+                if (status === "1" || status === 1) { // Awaiting
+                    viewModel.setProperty("/enabledPredictInput", true);
+                    viewModel.setProperty("/enabledOtherInputs", false);
+                } else if (status === "2" || status === 2) { // Ongoing
+                    viewModel.setProperty("/enabledPredictInput", false);
+                    viewModel.setProperty("/enabledOtherInputs", true);
+                } else { // Done
+                    const team1_score = object.team1_score;
+                    const team2_score = object.team2_score;
+                    const predicts = object.predicts;
+                    let enabledSaveBtn = (!isNaN(parseInt(team1_score)) && team1_score >= 0) && (!isNaN(parseInt(team2_score)) && team2_score >= 0) && (!isNaN(parseInt(predicts)) && predicts >= 0) ? true : false;
+                    viewModel.setProperty("/enabledSaveBtn", enabledSaveBtn);
+                }
             }
         }
     });
